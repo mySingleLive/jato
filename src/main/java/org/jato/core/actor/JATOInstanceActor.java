2c6a393dc9117431c54565139d272f2f7490e564
gongjun <dt_flys@hotmail.com>
2016-03-18 18:15:28 +0800

    change

--- src/main/java/org/jato/core/actor/JATOInstanceActor.java
+++ src/main/java/org/jato/core/actor/JATOInstanceActor.java
@@ -1,12 +1,15 @@
 package org.jato.core.actor;
 
 import com.esotericsoftware.reflectasm.MethodAccess;
+import org.jato.core.Main;
 import org.jato.core.furture.JATOFuture;
 import org.jato.core.message.MethodMessage;
 import org.jetlang.channels.Channel;
 import org.jetlang.fibers.Fiber;
+import sun.plugin.javascript.navig.Array;
 
-import java.util.Arrays;
+import java.awt.peer.ListPeer;
+import java.util.*;
 
 /**
  * [类注释]
@@ -22,11 +25,14 @@ public class JATOInstanceActor<T> extends JATOAbstractActor<MethodMessage> imple
 
     private MethodAccess methodAccess;
 
+    private Map<String, List<Object[]>> methodNameMap;
+
     public JATOInstanceActor(JATOActorFactory actorFactory, T instance, Channel<MethodMessage> mailbox, Fiber fiber) {
         super(actorFactory, mailbox, fiber);
         this.instance = instance;
         this.clazz = (Class<T>) instance.getClass();
         methodAccess = MethodAccess.get(clazz);
+        methodNameMap = buildMethodNameMap(methodAccess);
     }
 
     public Class<T> getClazz() {
@@ -38,6 +44,33 @@ public class JATOInstanceActor<T> extends JATOAbstractActor<MethodMessage> imple
         return instance;
     }
 
+
+    private Map<String, List<Object[]>> buildMethodNameMap(MethodAccess methodAccess) {
+        Map<String, List<Object[]>>  map = new HashMap<String, List<Object[]>>();
+
+        String[] methodNames = methodAccess.getMethodNames();
+        Class[][] parameterTypes = methodAccess.getParameterTypes();
+        for (int i = 0; i < methodNames.length; i++) {
+            String name = methodNames[i];
+            Class[] pTypes = parameterTypes[i];
+            List<Object[]> paramIndexList = map.get(name);
+            if (paramIndexList == null) {
+                paramIndexList = new ArrayList<Object[]>();
+                map.put(name, paramIndexList);
+            }
+            Object[] paramIndex = new Object[2];
+            List<Class[]> paramTypes = (List<Class[]>) paramIndex[0];
+            if (parameterTypes == null) {
+                paramTypes = new ArrayList<Class[]>();
+                paramIndex[0] = paramTypes;
+            }
+
+            paramIndexList.add(paramIndex);
+
+
+        }
+        return map;
+    }
 
 
 
@@ -103,6 +136,16 @@ public class JATOInstanceActor<T> extends JATOAbstractActor<MethodMessage> imple
         for (int i = 0, n = methodNames.length; i < n; i++)
             if (methodNames[i].equals(methodName) && matchTypes(paramTypes, parameterTypes[i])) return i;
         throw new IllegalArgumentException("Unable to find public method: " + methodName + " " + Arrays.toString(parameterTypes));
+
+/*
+        Object[] paramIndex = methodNameMap.get(methodName);
+        if (paramIndex != null) {
+            List<Class[]> parameterTypes = (List<Class[]>) paramIndex[0];
+            for (int i = 0, n = parameterTypes.size(); i < n; i++)
+                if (matchTypes(paramTypes, parameterTypes.get(i))) return i;
+        }
+        throw new IllegalArgumentException("Unable to find public method: " + methodName + " " + String.valueOf(parameterTypes));
+*/
     }
 
 
@@ -114,15 +157,20 @@ public class JATOInstanceActor<T> extends JATOAbstractActor<MethodMessage> imple
         Object[] args = message.getArgs();
         Class[] clzzes = message.getArgTypes();
         int index = getIndex(methodName, clzzes);
-        result = methodAccess.invoke(instance, index, args);
-        JATOFuture future = message.getFuture();
-        if (future == null) {
-            return;
-        }
         try {
-            future.set(result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            result = methodAccess.invoke(instance, index, args);
+            JATOFuture future = message.getFuture();
+            if (future == null) {
+                return;
+            }
+            try {
+                future.set(result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (Throwable t) {
+            System.out.println("index: " + index + ", args: " + Arrays.toString(args));
+            t.printStackTrace();
         }
     }
 
